fdbca62773d7898539dd780e59d6fd2641d0ce62
cosmin <doruivascu54@gmail.com>
2025-03-17 15:34:59 +0200

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "JOEL~XMD~5GVSiUmY",
+  SESSION_ID: process.env.SESSION_ID || "JOEL~XMD~StiYqSmd",
   PREFIX: process.env.PREFIX || '.',
 //let's add menu captions by lord joel
    BOT_NAME: process.env.BOT_NAME || "ᴊᴏᴇʟ xᴍᴅ ʙᴏᴛ ᴠ¹⁰",
